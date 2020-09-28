cf863f10ac1abd6bbf1aae6e9ec9ebf20f92084e
Andrew Rosário <andrew.arosario@gmail.com>
2020-09-27 21:34:26 -0300

    adaptando interaction strategy no factory

--- src/main/create-schedule-factory.ts
+++ src/main/create-schedule-factory.ts
@@ -1,8 +1,17 @@
 import { CreateSchedule } from "../domain/create-schedule";
 import { CreateScheduleUseCase } from "../usecases/create-schedule";
-import { ReadLineSyncAdapter } from "../adapters/implementations/ReadLineSyncAdapter";
+import { ReadLineSyncAdapter } from "../adapters/implementations/interaction/ReadLineSyncAdapter";
+import { VenomBotAdapter } from "../adapters/implementations/interaction/VenomBotAdapter";
+import { InteractionStrategy } from "../adapters/implementations/interaction/InteractionStrategy";
+import config from '../config/env'
 
 export const createScheduleFactory = (): CreateSchedule => {
-    const interaction = new ReadLineSyncAdapter()
+    const interactionStrategy = new InteractionStrategy(
+        config.interactionType,
+        new ReadLineSyncAdapter(),
+        new VenomBotAdapter()
+    )
+    const interaction = interactionStrategy.execute()
+    
     return new CreateScheduleUseCase(interaction)
 }
